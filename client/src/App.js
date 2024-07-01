63053eec07636e8c148453e3ce58ad1cb6faa8ab
Kumar Shresth <kumarshresth2004@gmail.com>
2024-07-01 18:14:16 +0530

    Made Successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react'
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [expenses, setexpenses] = useState([])
+  const [title, settitle] = useState('')
+  const [amount, setamount] = useState('')
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/expense')
+    .then(res=>setexpenses(res.data))
+    .catch(err=>console.log(err))
+  },[])
+
+  const addExpense = () =>{
+    axios.post('http://localhost:5000/api/expense',{title,amount})
+    .then(res=>setexpenses([...expenses],res.data))
+    .catch(err=>console.log(err))
+  }
+
+  const deleteExpense = (id)=>{
+    axios.delete(`http://localhost:5000/api/expense/${id}`)
+    .then(()=>setexpenses(expenses.filter(exp=> exp._id!==id)))
+    .catch(err=>console.log(err))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Expense Tracker</h1>
+      <div>
+        <input
+        type='text'
+        placeholder='Title'
+        value={title}
+        onChange={(e)=>settitle(e.target.value)}/>
+
+        <input
+        type='number'
+        placeholder='Amount'
+        value={amount}
+        onChange={(e)=>setamount(e.target.value)}/>
+
+        <button onClick={addExpense}>Add Expense</button>
+      </div>
+
+      <ul>
+        {expenses.map(exp=>(
+          <li key={exp._id}>
+            {exp.title} - ${exp.amount}
+            <button onClick={()=>deleteExpense(exp._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT||5000
 
-app.use(cors())
+const corsOptions = {
+    origin: 'http://localhost:3000', // Replace with your frontend's origin
+    optionsSuccessStatus: 200,
+  };
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
@@ -15,5 +20,5 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((err)=>console.log(err))
 
 app.get('/',(req,res)=>res.send('Expense Tracker API'))
-app.use('/api/expenses',require('./routes/expenseRouter'))
+app.use('/api/expense',require('./routes/expenseRouter'))
 app.listen(PORT,()=>console.log(`Server is listening at ${PORT}`))
